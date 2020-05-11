967b745ab0fff7819bc8e62bea8b26059674ca5c
Roland Tougan <miton-roland.tougan@etudiant.univ-rennes1.fr>
2020-05-11 16:04:05 +0200

    listesQuestions

--- inverse-qi/application/views/z_include_menu.php
+++ inverse-qi/application/views/z_include_menu.php
@@ -1,23 +1,26 @@
 <nav>
-    <div class="nav-wrapper blue darken-1">
-        <a href="/#" class="brand-logo center">Invers QI</a>
-        <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="<?php echo base_url('inverseQI/inverse-qi/./index.php/welcome'); ?>">Accueil</a></li>
-        </ul>
+	<div class="nav-wrapper blue darken-1">
+		<a href="<?php echo base_url('inverseQI/inverse-qi/./index.php/welcome'); ?>" class="brand-logo center">Invers QI</a>
+		<ul id="nav-mobile" class="left hide-on-med-and-down">
+			<li><a href="<?php echo base_url('inverseQI/inverse-qi/./index.php/welcome'); ?>">Accueil</a></li>
+		</ul>
 		<ul id="nav-mobile" class="left hide-on-med-and-down">
 			<li><a href="<?php echo base_url('inverseQI/inverse-qi/./index.php/Questions/listesQuestions'); ?>">Questions</a>
 			</li>
 		</ul>
-        <?php if(!isAuthenticated()) :?>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="<?php echo base_url('inverseQI/inverse-qi/./index.php/welcome/adminConnexion'); ?>">Connexion</a></li>
-            </ul>
-        <?php endif;?>
-        </ul>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <?php if(isAuthenticated()) :?>
-            <li><a href="/index.php/user/disconnect">Déconnexion</a></li>
-        <?php endif;?>
+		<?php if (!isAuthenticated()) : ?>
+			<ul id="nav-mobile" class="right hide-on-med-and-down">
+				<li><a href="<?php echo base_url('inverseQI/inverse-qi/./index.php/welcome/adminConnexion'); ?>">Connexion</a>
+				</li>
+			</ul>
+		<?php endif; ?>
+		</ul>
+		<ul id="nav-mobile" class="right hide-on-med-and-down">
+			<?php if (isAuthenticated()) : ?>
+				<li>
+					<a href="<?php echo base_url('inverseQI/inverse-qi/./index.php/user/disconnect'); ?>">Déconnexion</a>
+				</li>
+			<?php endif; ?>
 
-    </div>
+	</div>
 </nav>
